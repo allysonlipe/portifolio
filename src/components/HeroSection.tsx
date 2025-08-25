7c157bd8e7d48af3e65fc19469548d41f02e8acf
allysonlipe <allysongunner1@hotmail.com>
2025-08-24 21:23:45 -0300

    chore/hero-copy-change

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,7 +5,12 @@ export default function HeroSection() {
     <>
       <div className="grid grid-cols-2 gap-4 h-36 p-4 m-8">
         <div className="flex flex-col flex-wrap justify-center items-center">
-          <h1 className="text-4xl font-bold font-sans text-sky-300">Hi, I'm</h1>
+          <h1 className="text-4xl font-medium font-sans text-sky-400">
+            Hi, I'm
+          </h1>
+          <h1 className="text-4xl font-bold font-mono text-sky-100">
+            ALLYSON PHILIPE{" "}
+          </h1>
         </div>
         <div className=" flex justify-center items-center">
           <HeroLottie />
